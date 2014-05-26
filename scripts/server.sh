b5236fd11b56198874b52624c0d5ce97faeaa086
Karim Geiger <me@karim-geiger.de>
2014-05-26 19:35:52 +0200

    add server secret

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,7 +4,13 @@
 
 # VARIABLES
 SERVER_DOMAIN="example.com" # domain and path only, don't add http/https!
+SERVER_SECRET="secret" # server secret as configured in server script
 SERVER_PROTOCOL="http" # http or https
+
+SAVEFILE="/mnt/saveme"
+
+# DON'T CHANGE ANYTHING AFTER THIS LINE
+SERVER_BASE_URL="${SERVER_PROTOCOL}://${SERVER_DOMAIN}/?secret=${SERVER_SECRET}"
 
 # FUNCTIONS
 function validate {
@@ -12,12 +18,19 @@ function validate {
     command -v curl >/dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Aborting."; exit 1; }
 }
 
+function check_set_save_flag {
+    if [ $(curl -s "${SERVER_BASE_URL}&get=save") == "save" ]
+      then
+        touch "$SAVEFILE"
+    fi
+}
+
 validate
 while [ 1 ]
   do
     ping -c 1 "$SERVER_DOMAIN" > /dev/null 2>&1 && {
         # continue if server is reachable
-        echo "ok"
+        check_set_save_flag
     }
     sleep 60 # call the server every minute
 done
